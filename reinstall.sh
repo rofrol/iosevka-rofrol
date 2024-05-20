c170a6e21c71101425e01bdfaa11f88fb995a08c
Roman Frołow <rofrol@gmail.com>
2024-05-20 22:02:12 +0200

    IosevkaCustom

--- reinstall.sh
+++ reinstall.sh
@@ -4,8 +4,8 @@ DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo ${DIR}
 ln -sf ${DIR}/private-build-plans.toml ${DIR}/vendor/Iosevka/private-build-plans.toml &&
 	cd ${DIR}/vendor/Iosevka &&
-	npm run build -- ttf::IosevkaCustom &&
+	npm run build -- ttf-unhinted::IosevkaCustom &&
 	cd ${DIR}/vendor/FontPatcher &&
 	fontforge -script font-patcher --complete ${DIR}/vendor/Iosevka/dist/IosevkaCustom/TTF/IosevkaCustom-Regular.ttf &&
 	cp IosevkaCustomNerdFont-Regular.ttf ${DIR}/ &&
-	open ${DIR}/IosevkaCustomNerdFont-Regular.ttf
+	open -a Font\ Book ${DIR}/IosevkaCustomNerdFont-Regular.ttf
