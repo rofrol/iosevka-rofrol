4e00e73d8f79a068b41fe721a41148f8140c6638
Roman Frołow <rofrol@gmail.com>
2024-05-27 15:53:00 +0200

    IosevkaRofrolZed2 weight 450

--- reinstall_iosevka_rofrol_zed2.sh
+++ reinstall_iosevka_rofrol_zed2.sh
@@ -4,12 +4,13 @@ DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo ${DIR}
 ln -sf ${DIR}/private-build-plans.toml ${DIR}/vendor/Iosevka/private-build-plans.toml &&
 	cd ${DIR}/vendor/Iosevka &&
-	npm run build -- ttf-unhinted::iosevka-rofrol-zed2 &&
-	# open ${DIR}/vendor/Iosevka/dist/iosevka-rofrol-zed2/TTF-Unhinted/iosevka-rofrol-zed2-ExtendedMedium.ttf
+	# npm run build -- ttf-unhinted::iosevka-rofrol-zed2 &&
+	ls ${DIR}/vendor/Iosevka/dist/iosevka-rofrol-zed2/TTF-Unhinted/ &&
+	## open ${DIR}/vendor/Iosevka/dist/iosevka-rofrol-zed2/TTF-Unhinted/iosevka-rofrol-zed2-Regular.ttf
 	cd ${DIR}/vendor/FontPatcher &&
-	# --makegroups needed to have Postscript name https://github.com/ryanoasis/nerd-fonts/issues/579#issuecomment-1441612101
-	# Postscript name may be needed for kitty when disabling ligatures wiht -liga
-	# https://github.com/kovidgoyal/kitty/issues/2738#issuecomment-854384969
-	fontforge -script font-patcher --makegroups --complete ${DIR}/vendor/Iosevka/dist/iosevka-rofrol-zed2/TTF-Unhinted/iosevka-rofrol-zed2-ExtendedMedium.ttf &&
-	cp IosevkaRofrolZed2NerdFont-MediumExtended.ttf ${DIR}/ &&
-	open -a Font\ Book ${DIR}/IosevkaRofrolZed2NerdFont-MediumExtended.ttf
+	## --makegroups needed to have Postscript name https://github.com/ryanoasis/nerd-fonts/issues/579#issuecomment-1441612101
+	## Postscript name may be needed for kitty when disabling ligatures wiht -liga
+	## https://github.com/kovidgoyal/kitty/issues/2738#issuecomment-854384969
+	fontforge -script font-patcher --makegroups --complete ${DIR}/vendor/Iosevka/dist/iosevka-rofrol-zed2/TTF-Unhinted/iosevka-rofrol-zed2-Extended.ttf &&
+	cp IosevkaRofrolZed2NerdFont-Extended.ttf ${DIR}/ &&
+	open -a Font\ Book ${DIR}/IosevkaRofrolZed2NerdFont-Extended.ttf
